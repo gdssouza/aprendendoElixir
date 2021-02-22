c7b642f5a2dd0291a28a2662e343c23c01f106a6
Gustavo <gdssouza@id.uff.br>
2021-02-22 11:48:51 -0300

    Renomeado variáveis não utilizadas com _ no final

--- desafioStone.exs
+++ desafioStone.exs
@@ -8,15 +8,15 @@ defmodule Pagamentos do
   def soma([head|tail]), do: head + soma(tail)
 
   # recebe valor total e emails e retorna mapa
-  def mapa(valor, resto, []), do: %{}
+  def mapa(_valor, _resto, []), do: %{}
   def mapa(valor, 0, [head|tail] ), do: Map.put(mapa(valor, 0, tail), head, valor)
   def mapa(valor, resto, [head|tail] ), do: Map.put(mapa(valor, resto-1, tail), head, valor+1)
 
   # validacao
-  def desafio(listaCompras, []), do: %{}
-  def desafio([], listaEmails), do: %{}
+  def desafio(_listaCompras, []), do: %{}
+  def desafio([], _listaEmails), do: %{}
   # recebe compras e emails e retorna a resposta do teste
-  def desafio([itens, qtds, precos], listaEmails) do
+  def desafio([_itens, qtds, precos], listaEmails) do
     # valor total da compra
     vlTotal = soma(custos(qtds, precos))
     # quantidade de emails
